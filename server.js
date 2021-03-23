0126bcc10b245120a9203c581ec8c407f6aea37c
LAKKI <thakurluckysinghbrh@gmail.com>
2021-03-23 23:30:36 +0530

    +static folder;+npm packages

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+const path = require("path");
 const express = require('express');
 const app = express();
+require('dotenv').config()
+
+app.use(express.static(path.join(__dirname, "statics")));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('An alligator approaches!');
 });
 app.get('/lki', (req, res) => {
     res.send('Who is calling lakki; have a nice day');
-});
-app.use((req, res, next) => {
-    console.log('Time: ', Date.now());
-    next();
 });
 const PORT=process.env.PORT||3000
 app.listen(PORT, () => console.log('Spydrs Running on port 3000!'));
